bafe9f6425c9408264951a75fd2ed57e66c97aa0
Josh Humpherys <joshua.humpherys@gmail.com>
2016-09-21 19:48:27 -0400

    Disable Wi-Fi and cellular data
    Note: Cellular data is only disabled on API 20 or lower. Wi-Fi is always disabled. There is also a method to enable airplane mode, but it only works on API 16 or lower.

--- app/src/main/java/com/android/cubemaster/turnmeoff/MainActivity.java
+++ app/src/main/java/com/android/cubemaster/turnmeoff/MainActivity.java
@@ -1,11 +1,22 @@
 package com.android.cubemaster.turnmeoff;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
-        Toast.makeText(this, "TODO disable internet access", Toast.LENGTH_SHORT).show();
+        String message = "Wi-Fi has been disabled";
+        disableWifi();
+        if(Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            disableMobileData();
+            message = "Wi-Fi and cellular data have been disabled";
+        }
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+//        Only works up to API 16, but we can disable mobile data up to API 20, which is preferable
+//        activateAirplaneMode();
+    }
+
+    private void disableWifi() {
+        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(false);
+    }
+
+    private void disableMobileData() {
+        final ConnectivityManager conman = (ConnectivityManager)
+                getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        try {
+            final Class conmanClass = Class.forName(conman.getClass().getName());
+            final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
+            iConnectivityManagerField.setAccessible(true);
+            final Object iConnectivityManager = iConnectivityManagerField.get(conman);
+            final Class iConnectivityManagerClass = Class.forName(
+                    iConnectivityManager.getClass().getName());
+            final Method setMobileDataEnabledMethod = iConnectivityManagerClass
+                    .getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+            setMobileDataEnabledMethod.setAccessible(true);
+
+            setMobileDataEnabledMethod.invoke(iConnectivityManager, false);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void activateAirplaneMode() {
+        boolean successful = Settings.System.putInt(getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 1);
+        Log.d(LOG_TAG, String.valueOf(successful));
     }
 }
